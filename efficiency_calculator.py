8c7a381a2073cf8e3c9986f4ecf8bec0afdb1049
NehalH <nhosalikar@gmail.com>
2023-05-20 19:58:51 +0530

    Add Level 1 encoder and decoder

--- efficiency_calculator.py
+++ efficiency_calculator.py
@@ -1,2 +1,16 @@
-def eff_calculator(original, compressed):
-	print('Ratio : ', original/compressed)
+def calcu_efficiency(original_file, compressed_data):
+	original_size = os.path.getsize(original_file)
+	compressed_size = len(compressed_data)
+
+	print('=========================================')
+	print('Original file size : ', original_size)
+	print('Compressed file size : ', compressed_size)
+	print('Size difference : ', original_size - compressed_size)	
+	print('Percentage compression:', (1 - compressed_size / original_size) * 100, '%')
+	print('Magnitude compression:', original_size / compressed_size)
+	print('=========================================')
+
+	if original_size > compressed_size:
+		return True
+	
+	return False
--- run.py
+++ run.py
@@ -1,15 +1,10 @@
 import encoder_huffman_l1
 import decoder_huffman_l1
+import efficiency_calculator
 
 file_name = 'example.txt'
-
-l1_bin_file_suffix = 'encoded_l1_'
-l2_bin_file_suffix = 'encoded_l2_'
-
-l1_encoded_filename = l1_bin_file_suffix+file_name.split('.')[0]+'.bin'
 
 encoded_data, codes = encoder_huffman_l1.huffman_encode(file_name)
 decoded_data = decoder_huffman_l1.huffman_decode(encoded_data, codes)
 
-print(encoded_data)
-print(decoded_data)
+efficiency_calculator.calcu_efficiency(file_name, decoded_data)
